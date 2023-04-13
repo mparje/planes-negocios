13e2ef0258d7852a29fee750a5453e4286006b78
mparje <126131806+mparje@users.noreply.github.com>
2023-04-12 19:35:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ wish = st.text_input("¿Qué idea tienes? O deja en blanco y espera.")
 model_engine = "chatbot-1"
 prompt = (f"Hola, estoy interesado en crear una empresa basada en la idea '{wish}'. ¿Podrías ayudarme a crear un plan de negocios? Incluye nombre de la idea, una línea corta, persona objetivo del usuario, puntos de dolor del usuario a resolver, principales propuestas de valor, descripción detallada de la idea, canales de ventas y marketing, fuentes de ingresos por ventas, estructuras de costos, actividades clave, recursos clave, socios clave, pasos de validación de la idea, costo estimado del primer año de operación y desafíos comerciales potenciales a considerar."
 
-completions = openai.Completion.create(
+    completions = openai.Completion.create(
     engine=model_engine,
     prompt=prompt,
     max_tokens=1024,
